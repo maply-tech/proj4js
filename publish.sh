445ded8ae6a5a3bdb5d0d4257666bcc9b08a9ea8
Henrique Sobral Gomes <hsobral@gmail.com>
2026-07-02 16:01:58 -0300

    remove test browser
    Signed-off-by: Henrique Sobral Gomes <hsobral@gmail.com>

--- publish.sh
+++ publish.sh
@@ -11,11 +11,11 @@ git commit -m "build $VERSION"
 
 # Tag and push
 git tag -f v$VERSION -m "$VERSION"
-git push --tags git@github.com:proj4js/proj4js.git v$VERSION
+git push --tags git@github.com:maply-tech/proj4js.git v$VERSION
 
-# Publish
-npm publish
+# # Publish
+# npm publish
 
-# Cleanup
-git checkout master
-git branch -D build
+# # Cleanup
+# git checkout master
+# git branch -D build
